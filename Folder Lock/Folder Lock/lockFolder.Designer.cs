471a0c9803505a58f5e7351574437d8963f54001
HafiCode02 <hafizcode02@gmail.com>
2020-05-15 20:08:03 +0700

    Adding Lock Folder App

---
+++ Folder Lock/Folder Lock/lockFolder.Designer.cs
@@ -0,0 +1,330 @@
+﻿namespace Folder_Lock
+{
+    partial class lockFolder
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(lockFolder));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.urlBox1 = new System.Windows.Forms.TextBox();
+            this.Btn_CreateFolder = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.urlBox2 = new System.Windows.Forms.TextBox();
+            this.Btn_Browse = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.btn_Lock = new System.Windows.Forms.Button();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.btn_Unlock = new System.Windows.Forms.Button();
+            this.groupBox5 = new System.Windows.Forms.GroupBox();
+            this.logBox = new System.Windows.Forms.ListBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.createFolderToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.browseFolderToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.featureToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.clearLogToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.groupBox5.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.urlBox1);
+            this.groupBox1.Controls.Add(this.Btn_CreateFolder);
+            this.groupBox1.Location = new System.Drawing.Point(12, 30);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(503, 56);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Folder Creator";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // urlBox1
+            // 
+            this.urlBox1.Location = new System.Drawing.Point(136, 19);
+            this.urlBox1.Name = "urlBox1";
+            this.urlBox1.Size = new System.Drawing.Size(353, 20);
+            this.urlBox1.TabIndex = 1;
+            // 
+            // Btn_CreateFolder
+            // 
+            this.Btn_CreateFolder.Location = new System.Drawing.Point(7, 16);
+            this.Btn_CreateFolder.Name = "Btn_CreateFolder";
+            this.Btn_CreateFolder.Size = new System.Drawing.Size(110, 30);
+            this.Btn_CreateFolder.TabIndex = 0;
+            this.Btn_CreateFolder.Text = "Create Folder";
+            this.Btn_CreateFolder.UseVisualStyleBackColor = true;
+            this.Btn_CreateFolder.Click += new System.EventHandler(this.Btn_CreateFolder_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.urlBox2);
+            this.groupBox2.Controls.Add(this.Btn_Browse);
+            this.groupBox2.Location = new System.Drawing.Point(12, 92);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(503, 55);
+            this.groupBox2.TabIndex = 0;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Browse Folder to Lock/Unlock";
+            // 
+            // urlBox2
+            // 
+            this.urlBox2.Enabled = false;
+            this.urlBox2.Location = new System.Drawing.Point(136, 23);
+            this.urlBox2.Name = "urlBox2";
+            this.urlBox2.Size = new System.Drawing.Size(353, 20);
+            this.urlBox2.TabIndex = 2;
+            // 
+            // Btn_Browse
+            // 
+            this.Btn_Browse.Location = new System.Drawing.Point(7, 17);
+            this.Btn_Browse.Name = "Btn_Browse";
+            this.Btn_Browse.Size = new System.Drawing.Size(110, 30);
+            this.Btn_Browse.TabIndex = 1;
+            this.Btn_Browse.Text = "Browse";
+            this.Btn_Browse.UseVisualStyleBackColor = true;
+            this.Btn_Browse.Click += new System.EventHandler(this.Btn_Browse_Click);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.btn_Lock);
+            this.groupBox3.Location = new System.Drawing.Point(12, 153);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(117, 132);
+            this.groupBox3.TabIndex = 0;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Lock Folder";
+            // 
+            // btn_Lock
+            // 
+            this.btn_Lock.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btn_Lock.BackgroundImage")));
+            this.btn_Lock.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn_Lock.Location = new System.Drawing.Point(7, 16);
+            this.btn_Lock.Name = "btn_Lock";
+            this.btn_Lock.Size = new System.Drawing.Size(104, 110);
+            this.btn_Lock.TabIndex = 0;
+            this.btn_Lock.UseVisualStyleBackColor = true;
+            this.btn_Lock.Click += new System.EventHandler(this.btn_Lock_Click);
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.btn_Unlock);
+            this.groupBox4.Location = new System.Drawing.Point(148, 153);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(117, 132);
+            this.groupBox4.TabIndex = 0;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Unlock Folder";
+            // 
+            // btn_Unlock
+            // 
+            this.btn_Unlock.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btn_Unlock.BackgroundImage")));
+            this.btn_Unlock.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn_Unlock.Location = new System.Drawing.Point(6, 16);
+            this.btn_Unlock.Name = "btn_Unlock";
+            this.btn_Unlock.Size = new System.Drawing.Size(104, 110);
+            this.btn_Unlock.TabIndex = 1;
+            this.btn_Unlock.UseVisualStyleBackColor = true;
+            this.btn_Unlock.Click += new System.EventHandler(this.btn_Unlock_Click);
+            // 
+            // groupBox5
+            // 
+            this.groupBox5.Controls.Add(this.logBox);
+            this.groupBox5.Location = new System.Drawing.Point(283, 153);
+            this.groupBox5.Name = "groupBox5";
+            this.groupBox5.Size = new System.Drawing.Size(232, 149);
+            this.groupBox5.TabIndex = 0;
+            this.groupBox5.TabStop = false;
+            this.groupBox5.Text = "Log History";
+            this.groupBox5.Enter += new System.EventHandler(this.groupBox5_Enter);
+            // 
+            // logBox
+            // 
+            this.logBox.FormattingEnabled = true;
+            this.logBox.Location = new System.Drawing.Point(12, 18);
+            this.logBox.Name = "logBox";
+            this.logBox.Size = new System.Drawing.Size(209, 121);
+            this.logBox.TabIndex = 0;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.featureToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(544, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.createFolderToolStripMenuItem,
+            this.browseFolderToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // createFolderToolStripMenuItem
+            // 
+            this.createFolderToolStripMenuItem.Name = "createFolderToolStripMenuItem";
+            this.createFolderToolStripMenuItem.Size = new System.Drawing.Size(148, 22);
+            this.createFolderToolStripMenuItem.Text = "Create Folder";
+            this.createFolderToolStripMenuItem.Click += new System.EventHandler(this.createFolderToolStripMenuItem_Click);
+            // 
+            // browseFolderToolStripMenuItem
+            // 
+            this.browseFolderToolStripMenuItem.Name = "browseFolderToolStripMenuItem";
+            this.browseFolderToolStripMenuItem.Size = new System.Drawing.Size(148, 22);
+            this.browseFolderToolStripMenuItem.Text = "Browse Folder";
+            this.browseFolderToolStripMenuItem.Click += new System.EventHandler(this.browseFolderToolStripMenuItem_Click);
+            // 
+            // featureToolStripMenuItem
+            // 
+            this.featureToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.clearLogToolStripMenuItem});
+            this.featureToolStripMenuItem.Name = "featureToolStripMenuItem";
+            this.featureToolStripMenuItem.Size = new System.Drawing.Size(58, 20);
+            this.featureToolStripMenuItem.Text = "Feature";
+            // 
+            // clearLogToolStripMenuItem
+            // 
+            this.clearLogToolStripMenuItem.Name = "clearLogToolStripMenuItem";
+            this.clearLogToolStripMenuItem.Size = new System.Drawing.Size(124, 22);
+            this.clearLogToolStripMenuItem.Text = "Clear Log";
+            this.clearLogToolStripMenuItem.Click += new System.EventHandler(this.clearLogToolStripMenuItem_Click);
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 335);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(544, 22);
+            this.statusStrip1.TabIndex = 3;
+            this.statusStrip1.Text = "Lock Folder Apps Re-develop by Hafiz Caniago ";
+            this.statusStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.statusStrip1_ItemClicked);
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(271, 17);
+            this.toolStripStatusLabel1.Text = "Lock Folder Apps Re-Developed by Hafiz Caniago ";
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(12, 291);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(253, 23);
+            this.progressBar.TabIndex = 4;
+            // 
+            // lockFolder
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(544, 357);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox5);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "lockFolder";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Lock Folder Apps";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.lockFolder_FormClosing);
+            this.Load += new System.EventHandler(this.lockFolder_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox5.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.GroupBox groupBox5;
+        private System.Windows.Forms.Button Btn_CreateFolder;
+        private System.Windows.Forms.Button Btn_Browse;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem featureToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.TextBox urlBox1;
+        private System.Windows.Forms.TextBox urlBox2;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.Button btn_Lock;
+        private System.Windows.Forms.Button btn_Unlock;
+        private System.Windows.Forms.ListBox logBox;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.ToolStripMenuItem createFolderToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem browseFolderToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem clearLogToolStripMenuItem;
+    }
+}
+
---
+++ Folder Lock/Folder Lock/lockFolder.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Security.AccessControl;
+
+namespace Folder_Lock
+{
+    public partial class lockFolder : Form
+    {
+        private void progressBarLoading()
+        {
+            progressBar.Value = 10;
+            progressBar.Value = 20;
+            progressBar.Value = 30;
+            progressBar.Value = 40;
+            progressBar.Value = 50;
+            progressBar.Value = 60;
+            progressBar.Value = 70;
+            progressBar.Value = 80;
+            progressBar.Value = 90;
+            progressBar.Value = 100;
+        }
+
+        public lockFolder()
+        {
+            InitializeComponent();
+            urlBox1.Text = "D:\\Folder";
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox5_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void statusStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void Btn_CreateFolder_Click(object sender, EventArgs e)
+        {
+            /* Create Folder Button */
+            /* The First Code is to Make a Folder */
+            Directory.CreateDirectory(urlBox1.Text);
+            urlBox2.Text = urlBox2.Text;
+
+            /* Displaying ProgressBar Loading Effect */
+            progressBarLoading();
+
+            /* to Show Message */
+            MessageBox.Show(
+                "Folder Created & Ready to Lock",
+                "Lock Folder",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Information);
+
+            /* Reset ProgresBar*/
+            progressBar.Value = 0;
+        }
+
+        private void Btn_Browse_Click(object sender, EventArgs e)
+        {
+            /* Browse Folder Button */
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                urlBox2.Text = folderBrowserDialog1.SelectedPath;
+                progressBar.Value = 0;
+            }
+        }
+
+        private void btn_Lock_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                /* folder Path */
+                String folderPath = urlBox2.Text;
+                /* get Log on Windows Username */
+                String adminUsername = Environment.UserName;
+
+                /* get Access Control to path Folder*/
+                DirectorySecurity dSecurity = Directory.GetAccessControl(folderPath);
+                /* configure Access Control*/
+                FileSystemAccessRule fSAR = new FileSystemAccessRule(
+                    adminUsername, FileSystemRights.FullControl, AccessControlType.Deny);
+                /* adding permission ACL(Access Control List) in directory */
+                dSecurity.AddAccessRule(fSAR);
+                /* set access control */
+                Directory.SetAccessControl(folderPath, dSecurity);
+
+                /* run progressBar */
+                progressBarLoading();
+                /* show Message */
+                MessageBox.Show(
+                    "Folder Successfully Locked", "Folder Lock",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information
+                    );
+                /* reset progressbar */
+                progressBar.Value = 0;
+
+                /* adding Log Information*/
+                logBox.Items.Add(urlBox2.Text + "Locked on : ");
+                logBox.Items.Add(DateTime.Now.ToString());
+                logBox.Items.Add("");
+
+                /* Clear listBox */
+                urlBox2.Clear();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btn_Unlock_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string folderPath = urlBox2.Text;
+                string adminUsername = Environment.UserName;
+                DirectorySecurity dSecurity = Directory.GetAccessControl(folderPath);
+
+                /* get Access Control to path Folder*/
+                DirectorySecurity dSec = Directory.GetAccessControl(folderPath);
+                /* configure Access Control*/
+                FileSystemAccessRule fSAR = new FileSystemAccessRule(
+                    adminUsername, FileSystemRights.FullControl, AccessControlType.Deny);
+                /* remove access */
+                dSec.RemoveAccessRule(fSAR);
+                /* access control to folderpath */
+                Directory.SetAccessControl(folderPath, dSec);
+                /* loading progressbar */
+                progressBarLoading();
+
+                /* show Message */
+                MessageBox.Show(
+                    "Folder Successfully Unlocked", "Folder Lock",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information
+                    );
+                /* reset progressbar */
+                progressBar.Value = 0;
+
+                /* adding Log Information*/
+                logBox.Items.Add(urlBox2.Text + "Unlocked on : ");
+                logBox.Items.Add(DateTime.Now.ToString());
+                logBox.Items.Add("");
+
+                /* Clear listBox */
+                urlBox2.Clear();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void createFolderToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Btn_CreateFolder.PerformClick();
+        }
+
+        private void browseFolderToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Btn_Browse.PerformClick();
+        }
+
+        private void clearLogToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            logBox.Items.Clear();
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Re-Developed by Hafiz Caniago", "About Dev", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void lockFolder_Load(object sender, EventArgs e)
+        {
+            logBox.ForeColor = System.Drawing.Color.Blue;
+            try
+            {
+                string[] items = File.ReadAllLines(@"C:\\logs.hafcod");
+                logBox.Items.Clear();
+                logBox.Items.AddRange(items);
+                logBox.SelectedIndex = 0;
+            }
+            catch (System.Exception)
+            {
+
+            }
+        }
+
+        private void lockFolder_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            try
+            {
+                System.IO.StreamWriter sw = new System.IO.StreamWriter(@"C:\\logs.hafcod");
+                foreach (object item in logBox.Items)
+                {
+                    sw.WriteLine(item.ToString());
+                    sw.Close();
+                }
+            }
+            catch (System.Exception)
+            {
+
+            }
+
+        }
+    }
+}
