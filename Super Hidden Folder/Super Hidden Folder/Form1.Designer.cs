c1d7a79edbf4f36dd5658d6808377eae81afc48e
HafiCode02 <hafizcode02@gmail.com>
2020-05-17 02:29:18 +0700

    Adding Super Hidden Folder App

---
+++ Super Hidden Folder/Super Hidden Folder/Form1.Designer.cs
@@ -0,0 +1,289 @@
+﻿namespace Super_Hidden_Folder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.featureToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.textBoxPath1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_browse = new System.Windows.Forms.Button();
+            this.textboxPath2 = new System.Windows.Forms.TextBox();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.btn_hide = new System.Windows.Forms.Button();
+            this.btn_unhide = new System.Windows.Forms.Button();
+            this.groupBox5 = new System.Windows.Forms.GroupBox();
+            this.listboxlog = new System.Windows.Forms.ListBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.LoadingBar = new System.Windows.Forms.ToolStripProgressBar();
+            this.menuStrip1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.groupBox5.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.featureToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(518, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // featureToolStripMenuItem
+            // 
+            this.featureToolStripMenuItem.Name = "featureToolStripMenuItem";
+            this.featureToolStripMenuItem.Size = new System.Drawing.Size(58, 20);
+            this.featureToolStripMenuItem.Text = "Feature";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.aboutToolStripMenuItem.Text = "About";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btn_browse);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.textBoxPath1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 34);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(498, 45);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Browse Folder";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.textboxPath2);
+            this.groupBox2.Location = new System.Drawing.Point(12, 85);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(290, 44);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Folder Name to Hide / Unhide";
+            // 
+            // textBoxPath1
+            // 
+            this.textBoxPath1.Location = new System.Drawing.Point(86, 15);
+            this.textBoxPath1.Name = "textBoxPath1";
+            this.textBoxPath1.Size = new System.Drawing.Size(294, 20);
+            this.textBoxPath1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Folder Name  :";
+            // 
+            // btn_browse
+            // 
+            this.btn_browse.Location = new System.Drawing.Point(398, 13);
+            this.btn_browse.Name = "btn_browse";
+            this.btn_browse.Size = new System.Drawing.Size(91, 23);
+            this.btn_browse.TabIndex = 2;
+            this.btn_browse.Text = "Browse Folder";
+            this.btn_browse.UseVisualStyleBackColor = true;
+            this.btn_browse.Click += new System.EventHandler(this.btn_browse_Click);
+            // 
+            // textboxPath2
+            // 
+            this.textboxPath2.Location = new System.Drawing.Point(10, 16);
+            this.textboxPath2.Name = "textboxPath2";
+            this.textboxPath2.Size = new System.Drawing.Size(269, 20);
+            this.textboxPath2.TabIndex = 3;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.btn_hide);
+            this.groupBox3.Location = new System.Drawing.Point(13, 136);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(140, 140);
+            this.groupBox3.TabIndex = 3;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Hide Folder";
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.btn_unhide);
+            this.groupBox4.Location = new System.Drawing.Point(161, 136);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(140, 140);
+            this.groupBox4.TabIndex = 4;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Unhide Folder";
+            // 
+            // btn_hide
+            // 
+            this.btn_hide.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btn_hide.BackgroundImage")));
+            this.btn_hide.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn_hide.Location = new System.Drawing.Point(9, 19);
+            this.btn_hide.Name = "btn_hide";
+            this.btn_hide.Size = new System.Drawing.Size(125, 115);
+            this.btn_hide.TabIndex = 0;
+            this.btn_hide.UseVisualStyleBackColor = true;
+            this.btn_hide.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btn_unhide
+            // 
+            this.btn_unhide.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btn_unhide.BackgroundImage")));
+            this.btn_unhide.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn_unhide.Location = new System.Drawing.Point(7, 18);
+            this.btn_unhide.Name = "btn_unhide";
+            this.btn_unhide.Size = new System.Drawing.Size(125, 116);
+            this.btn_unhide.TabIndex = 1;
+            this.btn_unhide.UseVisualStyleBackColor = true;
+            this.btn_unhide.Click += new System.EventHandler(this.btn_unhide_Click);
+            // 
+            // groupBox5
+            // 
+            this.groupBox5.Controls.Add(this.listboxlog);
+            this.groupBox5.Location = new System.Drawing.Point(309, 86);
+            this.groupBox5.Name = "groupBox5";
+            this.groupBox5.Size = new System.Drawing.Size(201, 190);
+            this.groupBox5.TabIndex = 5;
+            this.groupBox5.TabStop = false;
+            this.groupBox5.Text = "Log History";
+            // 
+            // listboxlog
+            // 
+            this.listboxlog.FormattingEnabled = true;
+            this.listboxlog.Location = new System.Drawing.Point(8, 18);
+            this.listboxlog.Name = "listboxlog";
+            this.listboxlog.Size = new System.Drawing.Size(185, 160);
+            this.listboxlog.TabIndex = 0;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.LoadingBar});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 282);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(518, 22);
+            this.statusStrip1.TabIndex = 6;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(285, 17);
+            this.toolStripStatusLabel1.Text = "Super Hidden Folder Re-Developed by Hafiz Caniago";
+            this.toolStripStatusLabel1.Click += new System.EventHandler(this.toolStripStatusLabel1_Click);
+            // 
+            // LoadingBar
+            // 
+            this.LoadingBar.Name = "LoadingBar";
+            this.LoadingBar.Size = new System.Drawing.Size(210, 16);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(518, 304);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.groupBox5);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Super Hidden Folder";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox5.ResumeLayout(false);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem featureToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btn_browse;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxPath1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox textboxPath2;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button btn_hide;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.Button btn_unhide;
+        private System.Windows.Forms.GroupBox groupBox5;
+        private System.Windows.Forms.ListBox listboxlog;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripProgressBar LoadingBar;
+    }
+}
+
---
+++ Super Hidden Folder/Super Hidden Folder/Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+namespace Super_Hidden_Folder
+{
+    public partial class Form1 : Form
+    {
+        private void progressbarloading()
+        {
+            LoadingBar.Value = 5;
+            LoadingBar.Value = 10;
+            LoadingBar.Value = 15;
+            LoadingBar.Value = 20;
+            LoadingBar.Value = 25;
+            LoadingBar.Value = 30;
+            LoadingBar.Value = 35;
+            LoadingBar.Value = 40;
+            LoadingBar.Value = 45;
+            LoadingBar.Value = 50;
+            LoadingBar.Value = 55;
+            LoadingBar.Value = 60;
+            LoadingBar.Value = 65;
+            LoadingBar.Value = 70;
+            LoadingBar.Value = 75;
+            LoadingBar.Value = 80;
+            LoadingBar.Value = 85;
+            LoadingBar.Value = 90;
+            LoadingBar.Value = 95;
+            LoadingBar.Value = 100;
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listboxlog.ForeColor = System.Drawing.Color.Red;
+            try
+            {
+                string[] items = File.ReadAllLines(@"C:\\sfhlogs.hafcode");
+                listboxlog.Items.Clear();
+                listboxlog.Items.AddRange(items);
+                listboxlog.SelectedIndex = 0;
+            }
+            catch (System.Exception)
+            {
+
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textboxPath2.Text == string.Empty)
+            {
+                MessageBox.Show("textbox can not be empty", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                //membuat suatu perintah dengan bantuan script bat
+                string[] lines = { "attrib +h +s " + textboxPath2.Text };
+
+                //merekap perintah diatas dan menyimpannya menjadi sebuah file extensi .bat
+                System.IO.File.WriteAllLines(@"C:\Hide.bat", lines);
+
+                //mengeksekusi file yang telah dibuat di drive c
+                Process.Start("C:\\Hide.bat");
+
+                //menampilkan loading progressbar
+                progressbarloading();
+
+                //menampilkan kotak pesan
+                MessageBox.Show("Hide Folder in " + textBoxPath1.Text + "Success", "Super Folder Hidden", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                //merekap informasi dan menyimpannya kedalam listbox
+                listboxlog.Items.Add(textboxPath2.Text + " Hidden on " + DateTime.Now.ToString());
+
+                //Menghapus file bat yang berada di drive c
+                System.IO.File.Delete(@"C:\Hide.bat");
+            }
+        }
+
+        private void toolStripStatusLabel1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_browse_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBoxPath1.Text = folderBrowserDialog1.SelectedPath;
+                textboxPath2.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        private void btn_unhide_Click(object sender, EventArgs e)
+        {
+            //membuat perintah diatas dengan bantuan script .bat, perintah attrib digunakan untuk menampilkan atau memodifikasi sebuah attribut file
+            string[] lines = { "attrib -h -s" + textboxPath2.Text };
+
+            //merekap perintah diatas dan menyimpannya menjadi sebuah file dengan ektensi .bat untuk dieksekusi lebih lanjut
+            System.IO.File.WriteAllLines(@"C:\unHide.bat", lines);
+
+            //mengekseksi file bat yang telah dibuat di drive c
+            Process.Start("C:\\unHide.bat");
+
+            //menampilkan progressbar loading
+            progressbarloading();
+
+            //tampilkan kotak pesan
+            MessageBox.Show("Unhide Folder in " + textboxPath2.Text + "Success", "Super Folder Hidden", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            //merekap informasi menyimpannya ke dalam listbox
+            listboxlog.Items.Add(textboxPath2.Text + "un Hidden on" + DateTime.Now.ToString());
+
+            //menghapus file bat yang berada di drive c
+            System.IO.File.Delete(@"C:\unHide.bat");
+            System.IO.File.Delete(@"C:\Hide.bat");
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            try
+            {
+                System.IO.StreamWriter sw = new System.IO.StreamWriter(@"C:\\sfhlogs.hafcode");
+                foreach (object item in listboxlog.Items)
+                {
+                    sw.WriteLine(item.ToString());
+                    sw.Close();
+                }
+            }
+            catch (System.Exception)
+            {
+
+            }
+        }
+    }
+}
